844cb97d9b263daee9bc188084f1be3d2f5a8db6
fortesting9797 <fortesting9797@gmail.com>
2025-04-22 16:13:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
-          DevOps max-pro
+          DevOps max pro
         </p>
         <a
           className="App-link"
